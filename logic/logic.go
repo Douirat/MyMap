e287b919c27a363fa03d45cae59f6a4b96e7dc56
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2024-11-26 11:06:26 +0100

    BFS

--- logic/logic.go
+++ logic/logic.go
@@ -6,15 +6,22 @@ import "fmt"
 type City struct {
 	Id   int
 	Name string
+	Next *City
 }
 
 // Create a structure to reprsent a Graph:
-type CityRoads struct {
-	Size int
-	Map  [][]int
+type Cities struct {
+	Cities []*City
 }
 
-// Instantiate a new City:
+// Instantiate a new a graph:
+func NewGraph(size int) *Cities {
+	graph := new(Cities)
+	graph.Cities = make([]*City, size)
+	return graph
+}
+
+// Instantiate a new structure to represent the city:
 func NewCity(id int, name string) *City {
 	city := new(City)
 	city.Id = id
@@ -22,29 +29,60 @@ func NewCity(id int, name string) *City {
 	return city
 }
 
-// Instantiate the Graph:
-func MapInstance(size int) *CityRoads {
-	cityMap := new(CityRoads)
-	cityMap.Size = size
-	cityMap.Map = make([][]int, cityMap.Size)
-	for i := 0; i < cityMap.Size; i++ {
-		cityMap.Map[i] = make([]int, cityMap.Size)
+// Add a road between tow cities:
+func (cities *Cities) AddRoad(dep, des *City) {
+	if cities.Cities[dep.Id-1] == nil {
+		dep.Next = des
+		cities.Cities[dep.Id-1] = dep
+	} else {
+		temp := cities.Cities[dep.Id-1]
+		for temp.Next != nil {
+			temp = temp.Next
+		}
+		temp.Next = des
 	}
-	return cityMap
 }
 
-// Display the Map graph:
-func (roads *CityRoads) DisplayMap() {
-	fmt.Printf("  ")
-	for i := 0; i < roads.Size; i++ {
-		fmt.Printf("%d ", i+1)
-	}
-	fmt.Printf("\n")
-	for x:=0; x<roads.Size; x++{
-		fmt.Printf("%d ", x+1)
-		for z:=0; z<roads.Size; z++{
-			fmt.Printf("%d ", roads.Map[x][z])
+// Display the graph:
+func (cities *Cities) Display() {
+	for i := 0; i < len(cities.Cities); i++ {
+		fmt.Printf("%c.%d -----> ", i+97, i+1)
+		temp := cities.Cities[i]
+		for temp != nil {
+			fmt.Printf("%d, %s -> ", temp.Id, temp.Name)
+			temp = temp.Next
 		}
-		fmt.Println()
+		fmt.Printf("\n")
 	}
+}
+
+// Application of the breadth first search:
+func (cities *Cities)BFS(dep *City) {
+// Keep track of visited nodes: 
+visited := make(map[*City]bool)
+// Initialize a node with the depart node:
+Queue := []*City{dep}
+fmt.Println("BFS Traversal:")
+
+for len(Queue) > 0 {
+	// Dequeue the front of the queue:
+	node := Queue[0]
+	Queue = Queue[1:]
+
+	// skip if node have already been visited:
+	if visited[node] {
+		continue
+	}
+
+	// Mark as visited an process the node:
+	visited[node] = true
+	fmt.Printf("We are at point %s and node %d\n", node.Name, node.Id)
+	// Enqueue all non visited neigbors
+	for node != nil {
+		if !visited[node] {
+			Queue = append(Queue, node)
+		}
+		node = node.Next
+	}
+}
 }
--- main/main.go
+++ main/main.go
@@ -1,10 +1,24 @@
 package main
 
-import(
+import (
 	"github.com/Douirat/MyMap/logic"
 )
 
-func main(){
-	cityMap := logic.MapInstance(5)
-	cityMap.DisplayMap()
+func main() {
+	cityMap := logic.NewGraph(6)
+	a := logic.NewCity(1, "a")
+	b := logic.NewCity(2, "b")
+	c := logic.NewCity(3, "c")
+	d := logic.NewCity(4, "d")
+	e := logic.NewCity(5, "e")
+	f := logic.NewCity(6, "f")
+	cityMap.AddRoad(a, b)
+	cityMap.AddRoad(a, d)
+	cityMap.AddRoad(a, e)
+	cityMap.AddRoad(b, d)
+	cityMap.AddRoad(b, f)
+	cityMap.AddRoad(c, f)
+	cityMap.AddRoad(d, e)
+	// cityMap.Display()
+	cityMap.BFS(a)
 }
